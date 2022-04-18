82b68d36b567cb9328d392e5125a70f470d65055
bonnett93 <bonnettjordan@gmail.com>
2022-04-17 23:15:10 -0500

    Fix resolution message

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,5 +2,5 @@ export default function handleResponseFromAPI(promise) {
   return promise
     .then(() => ({ status: 200, body: 'success' }))
     .catch(() => Error())
-    .finally('Js has new concepts, some easy to understand, others not so much, but applying them to guides can be complex and unintuitive.');
+    .finally('Got a response from the API');
 }
