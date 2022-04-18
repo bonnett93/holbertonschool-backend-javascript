9bae9d7348745aedf9a1fb2da6abfb8578ffe35d
bonnett93 <bonnettjordan@gmail.com>
2022-04-18 01:48:20 -0500

    Create return array

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,7 +2,18 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const allsesponses = Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
-  /* return allsesponses.then((val) => console.log(val)) */
-  return allsesponses;
+  const allresponses = Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+  return allresponses.then((promises) => {
+    const promiseArray = [];
+    promises.forEach((response) => {
+      if (response.status === 'rejected') {
+        response.value = response.reason.toString();
+      }
+      const { status } = response;
+      const { value } = response;
+      promiseArray.push({ status, value });
+    });
+    console.log(promiseArray);
+    return promiseArray;
+  });
 }
